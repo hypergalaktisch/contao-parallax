520fd1efb0899b71e802a414fc9321ae4bf8b629
Georg Jaedicke <georg.jaedicke@gmail.com>
2017-09-13 17:51:25 +0200

    Bugfixes language, template and public path

--- src/Resources/contao/languages/en/tl_article.php
+++ src/Resources/contao/languages/en/tl_article.php
@@ -1,12 +1,12 @@
 <?php
 $GLOBALS['TL_LANG']['tl_article']['xlParallax_legend'] = "Parallax";
 
-$GLOBALS['TL_LANG']['tl_article']['xlParallaxAddImage'][0] = "Parallax-Hintergrund";
-$GLOBALS['TL_LANG']['tl_article']['xlParallaxAddImage'][1] = "Einen Parallax-Hintergrund für den Artikel hinzufügen";
+$GLOBALS['TL_LANG']['tl_article']['xlParallaxAddImage'][0] = "Parallax background";
+$GLOBALS['TL_LANG']['tl_article']['xlParallaxAddImage'][1] = "Add parallax background for article";
 
-$GLOBALS['TL_LANG']['tl_article']['xlParallaxSingleSRC'][0] = "Parallax-Bild auswählen";
-$GLOBALS['TL_LANG']['tl_article']['xlParallaxSingleSRC'][1] = "Parallax-Bild auswählen";
-$GLOBALS['TL_LANG']['tl_article']['xlParallaxPosition'][0] = "Position X und Y";
-$GLOBALS['TL_LANG']['tl_article']['xlParallaxPosition'][1] = "Position des Parallax-Bildes angeben (Beispiel: 10px, -30px, center). Es müssen immer beide Werte angegeben werden.";
+$GLOBALS['TL_LANG']['tl_article']['xlParallaxSingleSRC'][0] = "Select parallax image";
+$GLOBALS['TL_LANG']['tl_article']['xlParallaxSingleSRC'][1] = "Select parallax image";
+$GLOBALS['TL_LANG']['tl_article']['xlParallaxPosition'][0] = "Position x and y";
+$GLOBALS['TL_LANG']['tl_article']['xlParallaxPosition'][1] = "Set position for parallax image (Example: 10px, -30px, center). Both values must be set.";
 $GLOBALS['TL_LANG']['tl_article']['xlParallaxBleed'][0] = "Bleed";
-$GLOBALS['TL_LANG']['tl_article']['xlParallaxBleed'][1] = "Anzahl der Pixel die oberhalb und unterhalb überlappen. Dies kann langsame oder stotternde Scroll-Ereignisse in bestimmten Browsern verhindern.";
+$GLOBALS['TL_LANG']['tl_article']['xlParallaxBleed'][1] = "bleed above and below. Could fix scrolling issue.";

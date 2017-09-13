8746a4e657c062b0f735bd3a6354d1493c877be4
Georg Jaedicke <georg.jaedicke@gmail.com>
2017-09-13 17:35:07 +0200

    prepare and update for managed edition

---
+++ src/Resources/contao/languages/de/tl_article.php
@@ -0,0 +1,12 @@
+<?php
+$GLOBALS['TL_LANG']['tl_article']['xlParallax_legend'] = "Parallax";
+
+$GLOBALS['TL_LANG']['tl_article']['xlParallaxAddImage'][0] = "Parallax background";
+$GLOBALS['TL_LANG']['tl_article']['xlParallaxAddImage'][1] = "Add parallax background for article";
+
+$GLOBALS['TL_LANG']['tl_article']['xlParallaxSingleSRC'][0] = "Select parallax image";
+$GLOBALS['TL_LANG']['tl_article']['xlParallaxSingleSRC'][1] = "Select parallax image";
+$GLOBALS['TL_LANG']['tl_article']['xlParallaxPosition'][0] = "Position x and y";
+$GLOBALS['TL_LANG']['tl_article']['xlParallaxPosition'][1] = "Set position for parallax image (Example: 10px, -30px, center). Both values must be set.";
+$GLOBALS['TL_LANG']['tl_article']['xlParallaxBleed'][0] = "Bleed";
+$GLOBALS['TL_LANG']['tl_article']['xlParallaxBleed'][1] = "bleed above and below. Could fix scrolling issue.";
